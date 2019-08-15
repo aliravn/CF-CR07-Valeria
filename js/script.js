691acb03c61104df37a508fa5e47003e2cea791d
Valeria Nazarova <alira.vn@gmail.com>
2019-08-15 14:13:49 +0200

    create objects in array by calling class constructor, render the array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 // object LOcation created and dislayed
 class Location {
     constructor(name, city, zipCode, address, image) {
@@ -20,13 +22,12 @@ class Location {
     }
 }
 
-var place1 = new Location("Karlskirche", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg")
-$("#place").prepend(place1.display());
+var place1 = new Location("Karlskirche", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg");
+// $("#place").prepend(place1.display());
 
 console.log(place1.name);
 console.log(place1.showAddress());
 console.log(place1.teaserImage);
-
 
 // object Restaurant
 class Restaurant extends Location {
@@ -53,12 +54,13 @@ class Restaurant extends Location {
 }
 
 var rest1 = new Restaurant("Lemontree", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", "phoneNumber", "cusineType", "webPage")
-$("#rest").prepend(rest1.display());
+// $("#rest").prepend(rest1.display());
 
 console.log(rest1.name);
 console.log(rest1.showAddress());
 console.log(rest1.teaserImage);
 console.log(rest1.phoneNumber, rest1.cusineType, rest1.webPage);
+
 
 
 // object Event
@@ -86,9 +88,26 @@ class Event extends Location {
 }
 
 var event1 = new Event("BonJovi", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", "17.07.2019", "19:00", "80 EUR")
-$("#event").prepend(event1.display());
+// $("#event").prepend(event1.display());
 
 console.log(event1.name);
 console.log(event1.showAddress());
 console.log(event1.teaserImage);
 console.log(event1.date, event1.time, event1.ticketPrice);
+
+
+
+var renderList = [
+	new Location("Karlskirche", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg"),
+	new Location("Stephansdom", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg"),
+	new Restaurant("Lemontree", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", "phoneNumber", "cusineType", "webPage"),
+	new Restaurant("Tremble", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", "phoneNumber", "cusineType", "webPage"),
+	new Event("BonJovi", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", "17.07.2019", "19:00", "80 EUR"),
+	new Event("Rammstein", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", "17.07.2019", "19:00", "80 EUR")
+];
+
+console.log(renderList);
+
+renderList.forEach(function(item) {
+	$("#event").prepend(item.display());
+});
