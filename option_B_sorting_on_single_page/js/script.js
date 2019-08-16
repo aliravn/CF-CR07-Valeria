cc90bbc92acdabf17e0ba415328665e68cbaa508
Valeria Nazarova <alira.vn@gmail.com>
2019-08-16 23:39:46 +0200

    remove unused classes from js

--- option_B_sorting_on_single_page/js/script.js
+++ option_B_sorting_on_single_page/js/script.js
@@ -19,10 +19,10 @@ class Location {
 		return `<div class="col-12 col-md-6 col-lg-3 p-2">
 			<div class="col-border">
 				<img class="img-fluid img-thumbnail d-none d-md-block" src="${this.teaserImage}">
-				<h4 class="">${this.name}</h4>
-				<p class="">Created: ${this.timestamp}</p>
+				<h4>${this.name}</h4>
+				<p>Created: ${this.timestamp}</p>
 				<div class="text-container d-none d-md-block">
-					<p class="">${this.showAddress()}</p>
+					<p>${this.showAddress()}</p>
 				</div>
 			</div>	
 		</div>`
@@ -47,12 +47,12 @@ class Restaurant extends Location {
 <div class="col-12 col-md-6 col-lg-3 p-2">
 	<div class="col-border col-border-blue">
 		<img class="img-fluid img-thumbnail d-none d-md-block" src="${this.teaserImage}">
-		<h4 class="">${this.name}</h4>
-		<p class="">Created: ${this.timestamp}</p>
+		<h4>${this.name}</h4>
+		<p>Created: ${this.timestamp}</p>
 		<div class="text-container d-none d-md-block">
-			<p class="">${this.showAddress()}</p>
-			<p class="">${this.phoneNumber}</p>
-			<p class="">${this.cusineType}</p>
+			<p>${this.showAddress()}</p>
+			<p>${this.phoneNumber}</p>
+			<p>${this.cusineType}</p>
 			<a class="web-link" href="https://${this.webPage}" target="_blank">${this.webPage}</a>
 		</div>
 	</div>
@@ -77,13 +77,13 @@ class Event extends Location {
 		return `<div class="col-12 col-md-6 col-lg-3 p-2">
 			<div class="col-border col-border-orange">
 				<img class="img-fluid img-thumbnail d-none d-md-block" src="${this.teaserImage}">
-				<h4 class="">${this.name}</h4>
-				<p class="">Created: ${this.timestamp}</p>
+				<h4>${this.name}</h4>
+				<p>Created: ${this.timestamp}</p>
 				<div class="text-container d-none d-md-block">
-					<p class="">${this.showAddress()}</p>
-					<p class="">${this.date}</p>
-					<p class="">${this.time}</p>
-					<p class="">${this.ticketPrice}</p>
+					<p>${this.showAddress()}</p>
+					<p>${this.date}</p>
+					<p>${this.time}</p>
+					<p>${this.ticketPrice}</p>
 				</div>
 			</div>
 		</div>`
