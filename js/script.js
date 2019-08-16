91910a45e224187072065e1a6473e503cd09894f
Valeria Nazarova <alira.vn@gmail.com>
2019-08-16 12:59:38 +0200

    move classes back to main script.js file, update class constructor for automatical push into array

--- js/script.js
+++ js/script.js
@@ -1,20 +1,100 @@
+var renderList = [];
 //===================================================================
-// function to create random date for each object
+// constructor for basic class Location + 1 instance testing created
 //===================================================================
-// var start = new Date(2015, 0, 1); // year, month (as index), date
-// var end = new Date(); // current date
-// function randomDate(start, end) {
-//   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-//   console.log(randomDate);
-//   return `${randomDate.getDate()}.${randomDate.getMonth()+1}.${randomDate.getFullYear()} ${randomDate.getHours()}:${randomDate.getMinutes()}`;
-// }
-// console.log(randomDate(start, end));
+class Location {
+	constructor(name, city, zipCode, address, image, timestamp) {
+		  this.name = name;
+		  this.city = city;
+		  this.zipCode = zipCode;
+		  this.address = address;    
+		  this.teaserImage = image;
+		  this.timestamp = timestamp
+		  renderList.push(this);
+	}
 
+	showAddress() {
+		return `${this.address}, ${this.zipCode} ${this.city}`;
+	}
+	display() {
+		return `<div class="col-12 col-md-6 col-lg-3 p-2">
+			<div class="col-border">
+				<img class="img-fluid img-thumbnail col-border-thumbnail" src="${this.teaserImage}">
+				<h4 class="">${this.name}</h4>
+				<p class="">Created: ${this.timestamp}</p>
+				<div class="text-container">
+					<p class="">${this.showAddress()}</p>
+				</div>
+			</div>	
+		</div>`
+	}
+}
+
+//====================================================
+// class Restaurant  + 1 instance created for testing
+//====================================================
+class Restaurant extends Location {
+	constructor(name, city, zipCode, address, image, timestamp, phone, cusine, webPage) {
+		  super(name, city, zipCode, address, image, timestamp);
+		  this.phoneNumber = phone;
+		  this.cusineType = cusine;
+		  this.webPage = webPage;
+	}
+	showAddress() {
+		return `${super.showAddress()}`;
+	}
+	display() {
+		return `
+<div class="col-12 col-md-6 col-lg-3 p-2">
+	<div class="col-border">
+		<img class="img-fluid img-thumbnail col-border-thumbnail" src="${this.teaserImage}">
+		<h4 class="">${this.name}</h4>
+		<p class="">Created: ${this.timestamp}</p>
+		<div class="text-container">
+			<p class="">${this.showAddress()}</p>
+			<p class="">${this.phoneNumber}</p>
+			<p class="">${this.cusineType}</p>
+			<p class="">${this.webPage}</p>
+		</div>
+	</div>
+</div>`
+	}
+}
+
+//===============================================
+// class EVENT  + 1 instance created for testing
+//===============================================
+class Event extends Location {
+	constructor(name, city, zipCode, address, image, timestamp, date, time, ticketPrice) {
+		  super(name, city, zipCode, address, image, timestamp);
+		  this.date = date;
+		  this.time = time;
+		  this.ticketPrice = ticketPrice;
+	}
+	showAddress() {
+		return `${super.showAddress()}`;
+	}
+	display() {
+		return `<div class="col-12 col-md-6 col-lg-3 p-2">
+			<div class="col-border">
+				<img class="img-fluid img-thumbnail col-border-thumbnail" src="${this.teaserImage}">
+				<h4 class="">${this.name}</h4>
+				<p class="">Created: ${this.timestamp}</p>
+				<div class="text-container">
+					<p class="">${this.showAddress()}</p>
+					<p class="">${this.date}</p>
+					<p class="">${this.time}</p>
+					<p class="">${this.ticketPrice}</p>
+				</div>
+			</div>
+		</div>`
+	}
+}
 
 //===========================================================================
 // array of object, created using the class constructors in separate files
 //===========================================================================
-var renderList = [
+var randomList = [
 	new Location("bLocation1", "City", "PLZ", "Address", "img/test_Karlsplatz.jpg", `Mar 12 2012 16:00`),
 	new Location("fLocation2", "City", "PLZ", "Address", "img/test_Karlsplatz.jpg", `Mar 15 2012 10:00`),
 	new Location("aLocation3", "City", "PLZ", "Address", "img/test_Karlsplatz.jpg", `Mar 03 2012 10:00`),
@@ -28,6 +108,8 @@ var renderList = [
 	new Event("aEvent3", "City", "PLZ", "Address", "img/test_zoo.jpg", `Jun 12 2018 12:00`, "eventDate", "eventTime", "price"),
 	new Event("aEvent3", "City", "PLZ", "Address", "img/test_zoo.jpg", `Jun 12 2018 12:00`, "eventDate", "eventTime", "price")
 ];
+
+// var renderList = randomList.concat(placesList);
 
 //==============================================================================
 // function for rendering the objects from the array to display them in webpage - tested 16.08 / 10:00 working
@@ -56,3 +138,21 @@ $("#search-button").click(function(e){
 	var focusTarget = $("#search-field").val()
 	window.location.hash = `#${focusTarget}`;
 })
+
+
+
+
+
+
+
+//===================================================================
+// function to create random date for each object
+//===================================================================
+// var start = new Date(2015, 0, 1); // year, month (as index), date
+// var end = new Date(); // current date
+// function randomDate(start, end) {
+//   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+//   console.log(randomDate);
+//   return `${randomDate.getDate()}.${randomDate.getMonth()+1}.${randomDate.getFullYear()} ${randomDate.getHours()}:${randomDate.getMinutes()}`;
+// }
+// console.log(randomDate(start, end));
