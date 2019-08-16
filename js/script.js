6e8bb4b928531380acdb428d6fdbe9a27b3f3b78
Valeria Nazarova <alira.vn@gmail.com>
2019-08-16 09:47:27 +0200

    add more objects for resting

--- js/script.js
+++ js/script.js
@@ -10,126 +10,24 @@
 // }
 // console.log(randomDate(start, end));
 
-//===================================================================
-// constructor for basic class Location + 1 instance testing created
-//===================================================================
-// class Location {
-// 	constructor(name, city, zipCode, address, image, timestamp) {
-// 		  this.name = name;
-// 		  this.city = city;
-// 		  this.zipCode = zipCode;
-// 		  this.address = address;    
-// 		  this.teaserImage = image;
-// 		  this.timestamp = timestamp
-// 	}
-// 	showAddress() {
-// 		return `${this.address}, ${this.zipCode} ${this.city}`;
-// 	}
-// 	display() {
-// 		return `<div class="col-12 col-md-6 col-lg-3 p-2">
-// 			<div class="col-border">
-// 				<img class="img-fluid img-thumbnail col-border-thumbnail" src="${this.teaserImage}">
-// 				<h4 class="">${this.name}</h4>
-// 				<p class="">Created: ${this.timestamp}</p>
-// 				<div class="text-container">
-// 					<p class="">${this.showAddress()}</p>
-// 				</div>
-// 			</div>	
-// 		</div>`
-// 	}
-// }
-
-// var place1 = new Location("Karlskirche", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg");
-// $("#place").prepend(place1.display());
-// console.log(place1.name);
-// console.log(place1.showAddress());
-// console.log(place1.teaserImage);
-
-//====================================================
-// class Restaurant  + 1 instance created for testing
-//====================================================
-// class Restaurant extends Location {
-// 	constructor(name, city, zipCode, address, image, timestamp, phone, cusine, webPage) {
-// 		  super(name, city, zipCode, address, image, timestamp);
-// 		  this.phoneNumber = phone;
-// 		  this.cusineType = cusine;
-// 		  this.webPage = webPage;
-// 	}
-// 	showAddress() {
-// 		return `${super.showAddress()}`;
-// 	}
-// 	display() {
-// 		return `
-// <div class="col-12 col-md-6 col-lg-3 p-2">
-// 	<div class="col-border">
-// 		<img class="img-fluid img-thumbnail col-border-thumbnail" src="${this.teaserImage}">
-// 		<h4 class="">${this.name}</h4>
-// 		<p class="">Created: ${this.timestamp}</p>
-// 		<div class="text-container">
-// 			<p class="">${this.showAddress()}</p>
-// 			<p class="">${this.phoneNumber}</p>
-// 			<p class="">${this.cusineType}</p>
-// 			<p class="">${this.webPage}</p>
-// 		</div>
-// 	</div>
-// </div>`
-// 	}
-// }
-
-// var rest1 = new Restaurant("Lemontree", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", "phoneNumber", "cusineType", "webPage")
-// $("#rest").prepend(rest1.display());
-// console.log(rest1.display());
-// console.log(rest1.name);
-// console.log(rest1.showAddress());
-// console.log(rest1.teaserImage);
-// console.log(rest1.phoneNumber, rest1.cusineType, rest1.webPage);
-
-//===============================================
-// class EVENT  + 1 instance created for testing
-//===============================================
-// class Event extends Location {
-// 	constructor(name, city, zipCode, address, image, timestamp, date, time, ticketPrice) {
-// 		  super(name, city, zipCode, address, image, timestamp);
-// 		  this.date = date;
-// 		  this.time = time;
-// 		  this.ticketPrice = ticketPrice;
-// 	}
-// 	showAddress() {
-// 		return `${super.showAddress()}`;
-// 	}
-// 	display() {
-// 		return `<div class="col-12 col-md-6 col-lg-3 p-2">
-// 			<div class="col-border">
-// 				<img class="img-fluid img-thumbnail col-border-thumbnail" src="${this.teaserImage}">
-// 				<h4 class="">${this.name}</h4>
-// 				<p class="">Created: ${this.timestamp}</p>
-// 				<div class="text-container">
-// 					<p class="">${this.showAddress()}</p>
-// 					<p class="">${this.date}</p>
-// 					<p class="">${this.time}</p>
-// 					<p class="">${this.ticketPrice}</p>
-// 				</div>
-// 			</div>
-// 		</div>`
-// 	}
-// }
-
-
-// var event1 = new Event("BonJovi", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", "17.07.2019", "19:00", "80 EUR")
-// $("#event").prepend(event1.display());
-// console.log(event1.name);
-// console.log(event1.showAddress());
-// console.log(event1.teaserImage);
-// console.log(event1.date, event1.time, event1.ticketPrice);
-
 
 //============================================================
 // array of object, created using the above clss constructors
 //============================================================
 var renderList = [
-	new Location("Karlskirche", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg", `Mar 12 2012 10:00`),
-	new Restaurant("Lemontree", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", `May 21 2010 15:00`, "phoneNumber", "cusineType", "webPage"),
-	new Event("BonJovi", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", `Jan 21 2019 12:00`, "17.07.2019", "19:00", "80 EUR")];
+	new Location("Location1", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg", `Mar 12 2012 10:00`),
+	new Location("Location2", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg", `Mar 12 2012 10:00`),
+	new Location("Location3", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg", `Mar 12 2012 10:00`),
+	new Location("Location4", "Vienna", "1010", "Karlsplatz 1", "img/test_Karlsplatz.jpg", `Mar 12 2012 10:00`),
+	new Restaurant("Restaurant1", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", `May 21 2010 15:00`, "phoneNumber", "cusineType", "webPage"),
+	new Restaurant("Restaurant2", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", `May 21 2010 15:00`, "phoneNumber", "cusineType", "webPage"),
+	new Restaurant("Restaurant3", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", `May 21 2010 15:00`, "phoneNumber", "cusineType", "webPage"),
+	new Restaurant("Restaurant4", "Bagdad", "1150", "Kurbatra 3", "img/test_rest_sixta.png", `May 21 2010 15:00`, "phoneNumber", "cusineType", "webPage"),
+	new Event("Event1", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", `Jan 21 2019 12:00`, "17.07.2019", "19:00", "80 EUR"),
+	new Event("Event2", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", `Jan 21 2019 12:00`, "17.07.2019", "19:00", "80 EUR"),
+	new Event("Event3", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", `Jan 21 2019 12:00`, "17.07.2019", "19:00", "80 EUR"),
+	new Event("Event4", "Ernst Happel Stadion", "1020", "Stadionplatz 1", "img/test_zoo.jpg", `Jan 21 2019 12:00`, "17.07.2019", "19:00", "80 EUR")
+];
 // console.log(renderList);
 
 //==============================================================================
